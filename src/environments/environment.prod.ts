2d0ba9506773840a969c3bac04c17f32c8edbf54
alukard327 <alukard327@gmail.com>
2021-02-20 13:50:28 -0600

    Cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCQnxAPK76Qhhv-JaF6Ykx1o8JVt8q0yeI",
+    authDomain: "test-jaque.firebaseapp.com",
+    projectId: "test-jaque",
+    storageBucket: "test-jaque.appspot.com",
+    messagingSenderId: "533954440469",
+    appId: "1:533954440469:web:e920e8cd97547ebe0acd1c"
+  }
 };
